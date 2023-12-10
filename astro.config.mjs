f4352df868ceb09fddaf024533db327158cb7b1c
Robin Millette <robin@millette.info>
2023-12-10 15:35:35 -0500

    prefixDefaultLocale: true.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "fr"]
+    defaultLocale: "fr",
+    locales: ["en", "fr"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
   }
 });
